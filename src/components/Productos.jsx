60e5cb55628eadf532a842c402af2c6023747b20
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-06-13 22:41:03 +0200

    practica finalizada

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -3,28 +3,43 @@ import { useEffect, useState } from "react";
 export const Productos = () => {
 
   const [productos, setProductos] = useState([])
+  const [productoID, setProductoID] = useState(1)
 
   const fetchProductos = async () => {
     try {
-      const response = await fetch('https://fakestoreapi.com/products');
+      const response = await fetch(`https://fakestoreapi.com/products/${productoID}`);
       const data = await response.json();
       setProductos(data);
       console.log(data)
     } catch (error) {
-      
+      console.error( error )
     }
   }
 
   useEffect(() => {
     fetchProductos()
-  }, [])
+  } , [ productoID ])
 
+  const previusProduct = () => {
+    setProductoID( productoID - 1)
+  }
+
+  const nextProduct = () => {
+    setProductoID( productoID + 1)
+  }
+
+ 
   return (
     <>
       <h1>Productos</h1>
       <ul>
-      { productos.map( producto => <li key={producto.id}>{ producto.title }</li>)}
+      { <li>{ productos.title } </li> }
+      { <li>{ productos.price } </li> }
+      { <li>{ productos.description } </li> }
+      { <img src={productos.image} width={'100px'}/>}
       </ul>
+      <button onClick={ previusProduct }>Anterior</button>
+      <button onClick={ nextProduct }>Siguiente</button>
 
       
     </>
