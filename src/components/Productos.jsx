b22e2b7451d1173ca1e2fc98244664f8ee89dfde
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-06-14 00:00:35 +0200

    crearemos un custom hook

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,46 +1,9 @@
-import { useEffect, useState } from "react";
+import {useState} from 'react';
 
 export const Productos = () => {
-
-  const [productos, setProductos] = useState({})
-  const [productoID, setProductoID] = useState(1)
-
-  const fetchProductos = async () => {
-    try {
-      const response = await fetch(`https://fakestoreapi.com/products/${productoID}`);
-      const data = await response.json();
-      setProductos(data);
-      console.log(data)
-    } catch (error) {
-      console.error( error )
-    }
-  }
-
-  useEffect(() => {
-    fetchProductos()
-  } , [ productoID ])
-
-  const previusProduct = () => {
-    setProductoID( productoID - 1)
-  }
-
-  const nextProduct = () => {
-    setProductoID( productoID + 1)
-  }
-
- 
   return (
     <>
-      <h1>Productos</h1>
-      <ul>
-      { <li>{ productos.title } </li> }
-      { <li>{ productos.price } </li> }
-      { <li>{ productos.description } </li> }
-      { <img src={productos.image} width={'100px'}/>}
-      </ul>
-      <button onClick={ previusProduct }>Anterior</button>
-      <button onClick={ nextProduct }>Siguiente</button>
-
+      <h1>Listado de Productos:</h1>
       
     </>
   )
